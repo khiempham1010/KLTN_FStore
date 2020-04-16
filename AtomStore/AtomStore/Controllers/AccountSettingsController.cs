9bbf550c6c1478e6cfc9b268e7fc6f333a92349a
vtus727 <47789662+vtus727@users.noreply.github.com>
2020-04-16 15:06:24 +0700

    16/04/2020
    Update code:
    + Forgot password
    + Change password
    + Something UI others

---
+++ AtomStore/AtomStore/Controllers/AccountSettingsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AtomStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AtomStore.Controllers
+{
+    public class AccountSettingsController : Controller
+    {
+        [HttpGet]
+        [Route("accountsettings.html", Name = "AccountSettings")]
+        public IActionResult AccountSettings()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("accountsettings.html", Name = "AccountSettings")]
+        public async Task<IActionResult> AccountSettings(AccountSettingsViewModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ AtomStore/AtomStore/Models/AccountSettingsViewModel.cs
@@ -0,0 +1,35 @@
+﻿using AtomStore.Application.ViewModels.System;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AtomStore.Models
+{
+    public class AccountSettingsViewModel
+    {
+        public AppUserViewModel AppUserViewModel { get; set; }
+
+        [Display(Name = "Full name")]
+        public string FullName { set; get; }
+
+        [Display(Name = "DOB")]
+        [DataType(DataType.Date)]
+        public DateTime? BirthDay { set; get; }
+
+       
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+        [Display(Name = "Phone number")]
+        public string PhoneNumber { set; get; }
+
+        [Display(Name = "Avatar")]
+        public string Avatar { get; set; }
+    }
+}
