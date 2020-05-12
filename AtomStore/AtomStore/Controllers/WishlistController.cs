899db329bdda90e462838b88c46f5ebfba675e88
vtus727 <47789662+vtus727@users.noreply.github.com>
2020-05-12 14:47:49 +0700

    Add wishlist page

---
+++ AtomStore/AtomStore/Controllers/WishlistController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AtomStore.Controllers
+{
+    public class WishlistController : Controller
+    {
+        [HttpGet]
+        [Route("wishlist.html")]
+        public IActionResult Wishlist()
+        {
+            return View();
+        }
+    }
+}
