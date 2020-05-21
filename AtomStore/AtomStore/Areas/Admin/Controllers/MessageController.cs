078d22d19c65ec4b337ceeb33563e58b7d39e5f6
vtus727 <47789662+vtus727@users.noreply.github.com>
2020-05-21 12:03:16 +0700

    create chat admin

---
+++ AtomStore/AtomStore/Areas/Admin/Controllers/MessageController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AtomStore.Areas.Admin.Controllers
+{
+    public class MessageController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
