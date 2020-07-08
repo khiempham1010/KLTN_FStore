5d98abe009bf7f38f116fe0007675c8a97763224
vtus727 <47789662+vtus727@users.noreply.github.com>
2020-07-08 10:41:12 +0700

    Get button place order outside form tag in checkout page

--- AtomStore/AtomStore/wwwroot/client-app/controllers/checkout/index.js
+++ AtomStore/AtomStore/wwwroot/client-app/controllers/checkout/index.js
@@ -28,7 +28,7 @@
             else {
                 $('.CustomerEmail').css('border-color', '');
             }
-        });
+        }); 
         $('.CustomerAddress').focusout(function () {
             if ($('.CustomerAddress').val() == "") {
                 $('.CustomerAddress').css('border-color', '#ECA8A8');
@@ -37,75 +37,75 @@
                 $('.CustomerAddress').css('border-color', '');
             }
         });
-        $('#btnPaypal').on('click', function (e) {
-            e.preventDefault();
-            var customerName = $('.CustomerName').val();
-            var customerPhone = $('.CustomerPhone').val();
-            var customerEmail = $('.CustomerEmail').val();
-            var customerAddress = $('.CustomerAddress').val();
-            if (customerName == "" || customerPhone == "" || customerEmail == "" || customerAddress == "") {
-                if (customerAddress == "") {
-                    $('.CustomerAddress').focus();
-                    $('.CustomerAddress').css('border-color', '#ECA8A8');
-                }
-                else {
-                    $('.CustomerAddress').css('border-color', '');
-                }
-                if (customerEmail == "") {
-                    $('.CustomerEmail').focus();
-                    $('.CustomerEmail').css('border-color', '#ECA8A8');
-                }
-                else {
-                    $('.CustomerEmail').css('border-color', '');
-                }
-                if (customerPhone == "") {
-                    $('.CustomerPhone').focus();
-                    $('.CustomerPhone').css('border-color', '#ECA8A8');
-                }
-                else {
-                    $(".CustomerPhone").css('border-color', '');
-                }
-                if (customerName == "") {
-                    $('.CustomerName').focus();
-                    $('.CustomerName').css('border-color', '#ECA8A8');
-                }
-                else {
-                    $('.CustomerName').css('border-color', '');
-                }
-            }
-            else {
-                var customerMessage = $('.CustomerMessage').val();
-                var paymentMethod = 3;
-                $.ajax({
-                    url: '/Cart/CheckoutAjax',
-                    type: 'post',
-                    data: {
-                        CustomerName: customerName,
-                        CustomerPhone: customerPhone,
-                        CustomerAddress: customerAddress,
-                        CustomerEmail: customerEmail,
-                        CustomerMessage: customerMessage,
-                        PaymentMethod: paymentMethod
-                    },
-                    dataType: "json",
-                    beforeSend: function () {
-                        atom.startLoading();
-                    },
-                    success: function () {
-                        atom.notify('successful.', 'success');
-                        $('#frmCheckoutPaypal').submit();
-                        atom.stopLoading();
+        //$('#btnPaypal').on('click', function (e) {
+        //    e.preventDefault();
+        //    var customerName = $('.CustomerName').val();
+        //    var customerPhone = $('.CustomerPhone').val();
+        //    var customerEmail = $('.CustomerEmail').val();
+        //    var customerAddress = $('.CustomerAddress').val();
+        //    if (customerName == "" || customerPhone == "" || customerEmail == "" || customerAddress == "") {
+        //        if (customerAddress == "") {
+        //            $('.CustomerAddress').focus();
+        //            $('.CustomerAddress').css('border-color', '#ECA8A8');
+        //        }
+        //        else {
+        //            $('.CustomerAddress').css('border-color', '');
+        //        }
+        //        if (customerEmail == "") {
+        //            $('.CustomerEmail').focus();
+        //            $('.CustomerEmail').css('border-color', '#ECA8A8');
+        //        }
+        //        else {
+        //            $('.CustomerEmail').css('border-color', '');
+        //        }
+        //        if (customerPhone == "") {
+        //            $('.CustomerPhone').focus();
+        //            $('.CustomerPhone').css('border-color', '#ECA8A8');
+        //        }
+        //        else {
+        //            $(".CustomerPhone").css('border-color', '');
+        //        }
+        //        if (customerName == "") {
+        //            $('.CustomerName').focus();
+        //            $('.CustomerName').css('border-color', '#ECA8A8');
+        //        }
+        //        else {
+        //            $('.CustomerName').css('border-color', '');
+        //        }
+        //    }
+        //    else {
+        //        var customerMessage = $('.CustomerMessage').val();
+        //        var paymentMethod = 3;
+        //        $.ajax({
+        //            url: '/Cart/CheckoutAjax',
+        //            type: 'post',
+        //            data: {
+        //                CustomerName: customerName,
+        //                CustomerPhone: customerPhone,
+        //                CustomerAddress: customerAddress,
+        //                CustomerEmail: customerEmail,
+        //                CustomerMessage: customerMessage,
+        //                PaymentMethod: paymentMethod
+        //            },
+        //            dataType: "json",
+        //            beforeSend: function () {
+        //                atom.startLoading();
+        //            },
+        //            success: function () {
+        //                atom.notify('successful.', 'success');
+        //                $('#frmCheckoutPaypal').submit();
+        //                atom.stopLoading();
 
-                    },
-                    error: function () {
-                        atom.notify('failure', 'error');
-                        atom.stopLoading();
-                    }
-                });
-            }
+        //            },
+        //            error: function () {
+        //                atom.notify('failure', 'error');
+        //                atom.stopLoading();
+        //            }
+        //        });
+        //    }
            
             
-        });
+        //});
         $('#btnPlaceOrder').on('click', function () {
             var customerName = $('.CustomerName').val();
             var customerPhone = $('.CustomerPhone').val();
